57a19b9310de2a85b8592e4b378365780de86ac7
Shailesh <shailesh17mar@gmail.com>
2021-12-18 20:46:02 +0100

    Sidebar fix

--- src/presentation/modules/shared/components/layouts/sidebar.tsx
+++ src/presentation/modules/shared/components/layouts/sidebar.tsx
@@ -1,6 +1,11 @@
-import { slugify, EuiIcon, EuiPageSideBar } from "@elastic/eui";
+import {
+  slugify,
+  EuiIcon,
+  EuiPageSideBar,
+  EuiSideNavItemType,
+} from "@elastic/eui";
 import { Projects as Project } from "models";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, matchPath, useLocation, useNavigate } from "react-router-dom";
 import { Logo, SideNav } from "./layout.styles";
 
@@ -19,91 +24,106 @@ interface Props {
 }
 
 export const SideBar: React.FC<Props> = ({ projects, items, hasProjects }) => {
+  const [sideNav, setSideNav] = useState<EuiSideNavItemType<unknown>[]>();
   const [selectedItemName, setSelectedItem] = useState("");
   const navigate = useNavigate();
   const location = useLocation();
   const selectItem = (name: string) => {
     setSelectedItem(name);
   };
-  const createItem = (
-    name: string,
-    to: string,
-    data: any = {},
-    route?: string | string[]
-  ) => {
-    const match = route
-      ? Array.isArray(route)
-        ? route.find((path) =>
-            matchPath(
+
+  useEffect(() => {
+    const createItem = (
+      name: string,
+      to: string,
+      data: any = {},
+      route?: string | string[]
+    ) => {
+      const match = route
+        ? Array.isArray(route)
+          ? route.find((path) =>
+              matchPath(
+                {
+                  path: path,
+                },
+                location.pathname
+              )
+            )
+          : matchPath(
               {
-                path: path,
+                path: route,
               },
               location.pathname
             )
-          )
-        : matchPath(
-            {
-              path: route,
-            },
-            location.pathname
-          )
-      : false;
+        : false;
 
-    return {
-      id: slugify(name),
-      name,
-      isSelected: route ? selectedItemName === name || Boolean(match) : false,
-      onClick: () => {
-        selectItem(name);
-        if (to) navigate(to);
-      },
-      ...data,
+      return {
+        id: slugify(name),
+        name,
+        isSelected:
+          route !== undefined
+            ? selectedItemName === name || Boolean(match)
+            : false,
+        onClick: () => {
+          selectItem(name);
+          if (to) navigate(to);
+        },
+        ...data,
+      };
     };
-  };
 
-  const sideNav = [
-    createItem("", "", {
-      onClick: undefined,
-      items: items.map((item) =>
-        createItem(
-          item.label,
-          item.path,
-          {
-            icon: <EuiIcon color={item.color} size="l" type={item.icon} />,
-          },
-          item.route
-        )
-      ),
-    }),
-  ];
-  if (hasProjects) {
-    sideNav.push(
-      createItem("Projects", "", {
+    const sideNav = [
+      createItem("", "", {
         onClick: undefined,
-        items:
-          projects && projects.length > 0
-            ? projects.map((project) =>
-                createItem(
-                  project.name,
-                  `/projects/${project.id}`,
-                  {
-                    icon: (
-                      <EuiIcon color="#207af1" size="l" type="folderClosed" />
-                    ),
-                  },
-                  "/projects/:id"
+        items: items.map((item) =>
+          createItem(
+            item.label,
+            item.path,
+            {
+              icon: <EuiIcon color={item.color} size="l" type={item.icon} />,
+            },
+            item.route
+          )
+        ),
+      }),
+    ];
+    if (hasProjects) {
+      sideNav.push(
+        createItem("Projects", "", {
+          onClick: undefined,
+          items:
+            projects && projects.length > 0
+              ? projects.map((project) =>
+                  createItem(
+                    project.name,
+                    `/projects/${project.id}`,
+                    {
+                      icon: (
+                        <EuiIcon color="#207af1" size="l" type="folderClosed" />
+                      ),
+                    },
+                    "/projects/:id"
+                  )
                 )
-              )
-            : [
-                createItem("No Projects", "", {
-                  disabled: true,
-                }),
-              ],
-      })
-    );
-  }
+              : [
+                  createItem("No Projects", "", {
+                    disabled: true,
+                  }),
+                ],
+        })
+      );
+    }
+    setSideNav(sideNav);
+  }, [
+    hasProjects,
+    items,
+    location.pathname,
+    navigate,
+    projects,
+    selectedItemName,
+  ]);
 
-  return (
+  return sideNav ? (
     <EuiPageSideBar paddingSize="m">
       <SideNav
         aria-label="Liquid"
@@ -116,5 +136,5 @@ export const SideBar: React.FC<Props> = ({ projects, items, hasProjects }) => {
         items={sideNav}
       />
     </EuiPageSideBar>
-  );
+  ) : null;
 };
